91e73500d484cf55e5d5a6732f79a610a13ba5a1
saurabh-abes <kumar.saurabh488@gmail.com>
2017-01-22 01:36:58 +0530

    Implementing name calculator in angularjs

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 (function () {
-  angular.module('myFirstApp', [])
-  .controller('myFirstController',function ($scope) {
-
-    $scope.name="saurabh";
-    $scope.sayHello=function(){
-      return "Hello coursera";
+  'use strict';
+  angular.module('NameCalculator', [])
+  .controller('NameCalculatorController',function($scope){
+    $scope.name = "";
+    $scope.totalValue = 0;
+    $scope.displayNumeric = function(){
+      var totalNameValue = calculateNumericForString($scope.name);
+      $scope.totalValue = totalNameValue;
+    };
+    function calculateNumericForString(string){
+      var totalStringValue = 0;
+      for (var i = 0; i < string.length; i++) {
+        totalStringValue += string.charCodeAt(i);
+      }
+      return totalStringValue;
     }
+
   });
+
 })();
