5bd46fbd4c95a6fbbfe997877ff42b8c9b03a951
saurabh-abes <kumar.saurabh488@gmail.com>
2017-01-21 15:51:00 +0530

    Sharing data with the view through scope.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 (function () {
   angular.module('myFirstApp', [])
-  .controller('myFirstController',function () {
+  .controller('myFirstController',function ($scope) {
 
+    $scope.name="saurabh";
+    $scope.sayHello=function(){
+      return "Hello coursera";
+    }
   });
 })();
